c2abeb6b11c87bb03a839fa5f7481e2c0db85480
Papamani <30549690+Papamani@users.noreply.github.com>
2017-08-22 15:03:14 +0530

    evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter the number:");
+scanf("%d"&n);
+if(n/2==0)
+{
+printf("%d number is even");
+}
+else
+printf("%d number is odd");
+getch();
+}
