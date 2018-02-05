e5bf35f6cf1201e08fc8a2a2facf3fd8a6450c13
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-05 16:44:38 +0530

    27.c

---
+++ 27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int ret = 0;
+	char str1[] = "123";
+	char str2[] = "ABC";
+	ret = isNumericString(str1);
+	if(ret)
+		printf("It is numeric string");
+	else
+		printf("It is not numeric string")l;
+	ret = isNumericString(str2);
+	if(ret)
+		printf("It is numeric string");
+	else
+		printf("It is not numeric string");	
+	return 0;
+  }
