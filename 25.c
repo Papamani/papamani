3d7f50dd6b986403a4db6abff0742825f60cf5cb
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-05 16:21:41 +0530

    25.c

---
+++ 25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,n,i;
+printf("Enter the number of elements:");
+scanf("%d",&n);
+printf("Enter The elements are:");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+printf("%d",a);
+}
+return 0;
+}
