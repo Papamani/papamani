f179f2fe3c2ceb166184d8b9511ac580435a228a
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-05 16:39:27 +0530

    26.c

---
+++ 26.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,a,n;
+printf("Enter the number");
+scanf("%d",&n);
+printf("The elements are:%d",a);
+for (i = 0; i < a; i++)
+{
+scanf("%d", &arr_sort[i]);
+}
+return 0;
+}
