601147ab8d91a61e96041e2b65eb865acebd74d4
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-07 16:31:43 +0530

    30.c

---
+++ 30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int mins,hour,m,h;
+printf("Enter the hour :");
+scanf("%d",&hour);
+printf("Enter the minutes");
+scanf("%d",&mins);
+h=hour-1;
+m=mins-0;
+printf("%d,%d",h,m);
+print("/n 1 0");
+return 0;
+}
