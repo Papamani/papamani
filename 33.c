b49c6dd8951768bf982ec9c45da2a828ce09b2e0
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-09 16:36:11 +0530

    33.c

---
+++ 33.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str1[200],str;
+int len;
+printf("enter the string\n");
+scanf("%[^\n]s", s);
+if(str1[100]!=0)
+{
+len=str-len(str1[100]);
+}
+printf("%d",len);
+}
+}
+
+
+    
