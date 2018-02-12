ff17169d7ada923e139300b90626dc69e4f20118
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-12 15:58:53 +0530

    34.c

---
+++ 34.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int f,i;
+char str[100],l;
+printf("Enter the paragraph");
+scanf("%s",&p);
+for(i = 0; i <= l-1; i++)
+   {
+      if(str[i] == ' ')
+      {
+    f = f + 1;
+      }
+   }
+   printf("The number of lines in the string are %d", f);
+return 0;
+}
+
+	   
