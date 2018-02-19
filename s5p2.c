d624e10763b0199d0f764c4a8724f68e09c90140
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-19 16:46:56 +0530

    s5p2.c

--- s5p2.c
+++ s5p2.c
@@ -10,7 +10,7 @@ i=0;
 while (str1[i] == str2[i] && str1[i] != '\0')
 i++;
 if (str1[i] > str2[i])
-printf("%s > %s",str1.str2);
+printf("%s > %s",str1,str2);
 else if (str1[i] < str2[i])
 printf("%s < %s",str1,str2);
 else
