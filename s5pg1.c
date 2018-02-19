6814e3e7888dd8843d2ddca36e078c7dd354eee1
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-19 16:00:47 +0530

    s5pg1.c

---
+++ s5pg1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int n,i;
+printf("Enter the input string:\n");
+scanf("%s",&str);
+printf("Enter the number to be display the given number:\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+Printf("%s\n",str);
+}
+getch();
+}
