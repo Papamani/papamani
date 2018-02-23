0c2daa7d64de194ff2cca90d9c9c676dfb98bf89
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-23 16:57:05 +0530

    s5pg2.c

---
+++ s5pg2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number 1 t0 10");
+scanf("%d",&n);
+switch(n);
+{
+case 1:
+printf("\n one");
+break;
+case 2:
+printf("\n two");
+break;
+case 3:
+printf("\n three");
+break;
+case 4:
+printf("\n four");
+break;
+case 5:
+printf("\n five");
+break;
+case 6:
+printf("\n six");
+break;
+case 7:
+printf("\n seven");
+break;
+case 8:
+printf("\n eight");
+break;
+case 9:
+printf("\n nine");
+break;
+case 10:
+printf("\n ten");
+default:
+printf("Enter the number 1 to 10");
+}
+return 0;
+}
