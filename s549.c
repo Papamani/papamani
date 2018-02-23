dcba898bb9760085f5fc07c20991c13d12cc8fca
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-23 16:16:56 +0530

    s549.c

---
+++ s549.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter the number 1:");
+scanf("%d",&a);
+printf("Enter the number 2:");
+scanf("%d",&b);
+sum=a+b;
+printf("The sum of given numbers is %d",sum);
+return 0;
+}
