288ed532c55a2a39ac84fd7427499b211c64309a
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-26 16:28:57 +0530

    s6pg6.c

---
+++ s6pg6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,m,product;
+printf("Enter the two unmbers:\n");
+scanf("%d %d",&n,&m);
+product=n*m;
+if(product%2==0)
+{
+printf("It is even number\n")
+}
+else
+{
+printf("It is odd number\n");
+}
+return o;
+}
