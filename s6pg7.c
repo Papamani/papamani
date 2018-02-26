26d52a0f6210d18f8c7d13071a72d1d2f60c3303
Papamani <30549690+Papamani@users.noreply.github.com>
2018-02-26 16:57:10 +0530

    s6pg7.c

---
+++ s6pg7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void swap(int,int);
+int main()
+{
+int a,b;
+printf("Enter the a value");
+scanf("%d",&a);
+printf("Enter the b value");
+scanf("%d",&b);
+printf("Before swaping:");
+scanf("%d%d",&a,&b);
+printf("After swaping %d %d",a,b);
+return 0;
+}
+void main(x,y)
+{
+int temp;
+temp=x;
+x=y;
+y=temp;
+}
+
+
+
