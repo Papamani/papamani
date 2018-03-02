1b683f65bf653b3512063ab6e5dd542db98fa74d
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-02 15:54:18 +0530

    s7pg2.c

---
+++ s7pg2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char s[10];
+scanf("%s",s);
+int i,t=0,m;
+m=strlen(s)
+for(i=0;i<m;i++
+{
+if(a[i]==0 || a[i]==1;
+{
+t=1;
+}
+else
+{
+t=0;
+break();
+}
+}
+if(t==1)
+{
+printf("YEs");
+}
+}
+
+
