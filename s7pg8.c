af36c430cfad5942fe1c8f059083e4372a64396b
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-05 16:38:35 +0530

    s7pg8.c

---
+++ s7pg8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("It is multiple of 7\n");
+}
+else
+{
+printf("It is not multiple of 7\n");
+}
+return 0;
+}
