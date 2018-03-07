7b3c0328c9e23ce27b54819dfc71177b90a8e969
Papamani <30549690+Papamani@users.noreply.github.com>
2018-03-07 16:14:14 +0530

    s8pg3.c

---
+++ s8pg3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number :");
+scanf(%d",&n);
+printf("Enter the left and right value");
+scanf("%d%d",&a,&b);
+if(n>a && n<b)
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+return 0;
+}
